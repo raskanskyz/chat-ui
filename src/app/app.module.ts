850e8a3c8e2a5a4e592e3ec194b44ce6b3249778
Eran <eran@cream-software.com>
2018-01-10 20:55:02 +0200

    added avatar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//COMPONENTS
 import { AppComponent } from './app.component';
 import { ChatModalComponent } from './chat-modal/chat-modal.component';
 import { ModalSidebarComponent } from './modal-sidebar/modal-sidebar.component';
 import { ModalMainviewComponent } from './modal-mainview/modal-mainview.component';
 import { ChatListComponent } from './chat-list/chat-list.component';
 import { ChatListItemComponent } from './chat-list-item/chat-list-item.component';
+import { AvatarComponent } from './avatar/avatar.component';
+
+//SERVICES
 import { ChatRoomsService } from './services/chat-rooms.service';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,10 +21,12 @@ import { ChatRoomsService } from './services/chat-rooms.service';
     ModalSidebarComponent,
     ModalMainviewComponent,
     ChatListComponent,
-    ChatListItemComponent
+    ChatListItemComponent,
+    AvatarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [
     ChatRoomsService
